d23ddfcf2bbe950032ca24412537ba295f99c688
kenji mah <36205196+KenjiMah@users.noreply.github.com>
2025-05-30 16:02:11 -0700

    push

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,11 @@ export const projects = [
   {
     title: "Metal Letter",
     description: "Beginner metal working project for learning metalshop tools",
-    image: "/src/assets/images/metalLetter/Letter.jpeg",
+    image: "src/assets/images/metalLetter/Letter.jpeg",
   },
   {
     title: "Engraved Nameplate",
     description: "High-speed engraving with custom font.",
-    image: "/src/assets/images/maedaCrest/Crest.jpeg",
+    image: "src/assets/images/maedaCrest/Crest.jpeg",
   },
 ];

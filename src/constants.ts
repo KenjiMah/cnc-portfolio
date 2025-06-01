7ab6f06e3ffe67877060f816b02a4c72451e64c4
KenjiMah <36205196+KenjiMah@users.noreply.github.com>
2025-06-01 02:35:31 -0700

    added in the projects

--- src/constants.ts
+++ src/constants.ts
@@ -15,7 +15,7 @@ export const projects = [
     projectType: "Case Study",
     description:
       "Precision practice for facing a perfect cube with -0.0003 tolerance",
-    image: imageMap["./assets/images/dice/dice.jpeg"],
+    image: imageMap["./assets/images/dice/progress2.jpeg"],
   },
   {
     id: "metalLetter",
@@ -30,6 +30,14 @@ export const projects = [
     projectType: "Case Study",
     description: "Beginner metal working project for learning metalshop tools.",
     image: imageMap["./assets/images/maedaCrest/Crest.jpeg"],
+  },
+  {
+    id: "poroKeycaps",
+    title: "3D Printed Keycaps",
+    projectType: "SLA 3D Printing",
+    description:
+      "Printed some cute looking keycaps using the ELEGOO Saturn 2 MSLA 3D Printer.",
+    image: imageMap["./assets/images/poroKeycaps/poroKeycaps.jpeg"],
   },
   {
     id: "parklet",
